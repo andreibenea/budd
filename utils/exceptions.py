9f53761614c27d16afb75abd1c1f524b541357c6
Andrei Benea <andrei.benea89@gmail.com>
2025-12-05 19:04:09 -0500

    potential custom errors

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,4 +1,26 @@
 class InsufficientFundsError(Exception):
     """Raised when expense would make balance negative"""
+
     def __init__(self, message="Entering negative balance!"):
+        self.message = message
+
+
+class InvalidYearError(Exception):
+    """Raised when inputted year is not valid"""
+
+    def __init__(self, message="Invalid year!"):
+        self.message = message
+
+
+class InvalidMonthError(Exception):
+    """Raised when inputted month is not valid"""
+
+    def __init__(self, message="Invalid month!"):
+        self.message = message
+
+
+class InvalidDayError(Exception):
+    """Raised when inputted day is not valid"""
+
+    def __init__(self, message="Invalid day!"):
         self.message = message
